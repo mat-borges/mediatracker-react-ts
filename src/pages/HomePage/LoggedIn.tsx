6477ebeaf27429a4b224a697968f72cbf182d190
Mateus Borges <borgesm.work@gmail.com>
2023-04-19 13:36:08 -0300

    feat: calendar grid

---
+++ src/pages/HomePage/LoggedIn.tsx
@@ -0,0 +1,100 @@
+import { checkLeapYear, monthName, weekdayName } from '../../constants/dates';
+
+import { CSSProperties } from 'react';
+import styled from 'styled-components';
+
+export default function HomeLoggedIn() {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+
+  const daysOfTheMonth = checkLeapYear(month + 1, year);
+
+  function renderCalendar() {
+    return daysOfTheMonth.map((day) => {
+      const dayOfMonth = day.getDate();
+      const isCurrentMonth = day.getMonth() === month;
+
+      console.log(isCurrentMonth);
+
+      if (isCurrentMonth) {
+        return (
+          <Day key={dayOfMonth} isCurrentMonth={isCurrentMonth}>
+            <DayTitle>
+              <h1>{dayOfMonth}</h1>
+              <h2>{weekdayName(day.getDay())}</h2>
+            </DayTitle>
+            <DayContent>
+              <div>aaaaaaaaaaaaaaaa</div>
+              <div>bbbbbbbbbbbbbbbb</div>
+              <div>cccccccccccccccc</div>
+            </DayContent>
+          </Day>
+        );
+      } else {
+        return <div key={`${dayOfMonth}none`}></div>;
+      }
+    });
+  }
+
+  return (
+    <main>
+      <h1>{monthName(month)}</h1>
+      <Calendar>{renderCalendar()}</Calendar>
+    </main>
+  );
+}
+
+interface DayProps {
+  slot?: string;
+  style?: CSSProperties;
+  title?: string;
+  isCurrentMonth: boolean;
+}
+
+const Calendar = styled.div`
+  display: grid;
+  width: 100%;
+  height: 100%;
+  color: white;
+  background-color: black;
+  gap: 10px;
+  grid-template-columns: repeat(7, minmax(150px, 1fr));
+  grid-template-rows: repeat(6, minmax(300px, 1fr));
+`;
+
+const Day = styled.div<DayProps>`
+  display: ${(props) => (props.isCurrentMonth ? 'flex' : 'none')};
+  flex-direction: column;
+  height: 100%;
+  padding: 1px 1px 6px 1px;
+  margin: 6px 5px 0 0;
+  border-radius: 10px;
+  background-color: grey;
+`;
+
+const DayTitle = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  padding: 6px;
+  border-radius: 10px 10px 0 0;
+  background-color: darkgrey;
+`;
+
+const DayContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10px;
+  width: 100%;
+  height: 90%;
+  div {
+    min-height: 90px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 80%;
+  }
+`;
