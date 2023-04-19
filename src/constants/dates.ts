add0fd2fc3f73b66db55ed929de6660c82352d83
Mateus Borges <borgesm.work@gmail.com>
2023-04-19 13:35:21 -0300

    feat: functions to manipulate dates file

---
+++ src/constants/dates.ts
@@ -0,0 +1,67 @@
+function checkLeapYear(month: number, year: number = new Date().getFullYear()) {
+  const daysInMonth = new Date(year, month, 0).getDate();
+  const firstDayOfMonth = new Date(year, month - 1, 1).getDay();
+  const lastDayOfMonth = new Date(year, month - 1, daysInMonth).getDay();
+
+  const daysBefore = firstDayOfMonth === 0 ? 7 : firstDayOfMonth;
+  const daysAfter = lastDayOfMonth === 0 ? 0 : 7 - lastDayOfMonth;
+
+  const numDays = daysBefore + daysInMonth + daysAfter;
+  const firstDate = new Date(year, month - 1, 1 - daysBefore);
+  const dates = Array.from({ length: numDays }, (_, i) => new Date(firstDate.getTime() + i * 24 * 60 * 60 * 1000));
+  return dates;
+}
+
+function weekdayName(weekday: number) {
+  switch (weekday) {
+    case 0:
+      return 'Domingo';
+    case 1:
+      return 'Segunda';
+    case 2:
+      return 'Terça';
+    case 3:
+      return 'Quarta';
+    case 4:
+      return 'Quinta';
+    case 5:
+      return 'Sexta';
+    case 6:
+      return 'Sábado';
+    default:
+      return '';
+  }
+}
+
+function monthName(month: number) {
+  switch (month) {
+    case 0:
+      return 'Janeiro';
+    case 1:
+      return 'Fevereiro';
+    case 2:
+      return 'Março';
+    case 3:
+      return 'Abril';
+    case 4:
+      return 'Maio';
+    case 5:
+      return 'Junho';
+    case 6:
+      return 'Julho';
+    case 7:
+      return 'Agosto';
+    case 8:
+      return 'Setembro';
+    case 9:
+      return 'Outubro';
+    case 10:
+      return 'Novembro';
+    case 11:
+      return 'Dezembro';
+    default:
+      return '';
+  }
+}
+
+export { checkLeapYear, monthName, weekdayName };
