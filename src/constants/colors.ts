901264a64a1388fd53f74e75e1ddd55ec81557c2
Mateus Borges <borgesm.work@gmail.com>
2023-04-21 18:47:37 -0300

    refactor: all color to hex

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,11 +1,12 @@
-export const mainColor = "#898998";
-export const secondaryColor = "#e5e5e5";
-export const accentColor = "#f64348";
+export const mainColor = '#898998';
+export const secondaryColor = '#e5e5e5';
+export const accentColor = '#f64348';
 
-export const darkMainColor = "#1f1f1f";
-export const darkSecondaryColor = "#343434";
-export const darkAccentColor = "#404040";
+export const darkMainColor = '#1f1f1f';
+export const darkSecondaryColor = '#343434';
+export const darkAccentColor = '#404040';
+export const inputBackColor = '#D9D9D9';
 
-export const fontColor = "RGBA(#FFF, 0.96)";
-export const fontSecondayColor = "RGBA(#FFF, 0.67)";
-export const fontAccentColor = "RGBA(#FFF, 0.45)";
+export const fontColor = '#F7F7F7';
+export const fontSecondayColor = '#BCBCBC';
+export const fontAccentColor = '#909090';
