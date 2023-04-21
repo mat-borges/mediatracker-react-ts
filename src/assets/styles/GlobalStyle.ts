0f3d8cbb9489e69e7891e4f38e1a84084417c1a9
Mateus Borges <borgesm.work@gmail.com>
2023-04-21 18:46:38 -0300

    feat: import fonts

--- src/assets/styles/GlobalStyle.ts
+++ src/assets/styles/GlobalStyle.ts
@@ -3,7 +3,7 @@ import { accentColor, fontColor, mainColor, secondaryColor } from '../../constan
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&family=Orbitron:wght@400;500;600;700;800;900&display=swap');
 
 *{
 	box-sizing: border-box;
@@ -51,7 +51,7 @@ body {
 	color: ${fontColor};
 	font-size: 16px;
 	line-height: 1.2;
-	font-family: 'Nunito', sans-serif;
+	font-family: 'Nunito Sans', sans-serif;
 	background-color: ${mainColor};
 }
 ol, ul {
